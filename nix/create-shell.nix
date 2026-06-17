0097e3a0bc68fd44fa456e480f8972275ee6e80d
Leo Moser <leomoser99@gmail.com>
2026-06-17 14:23:36 +0200

    Add Python environment to Nix shell
    Signed-off-by: Leo Moser <leomoser99@gmail.com>

--- nix/create-shell.nix
+++ nix/create-shell.nix
@@ -2,20 +2,38 @@
   extra-packages ? [],
 }: ({
   lib,
-  klayout,
+  klayout-app,
   coreutils,
+  python3,
   devshell,
 }: let
+  python-env = (
+    python3.withPackages (
+      pp:
+      (
+        [python3.pkgs.klayout]
+      )
+    )
+  );
+  python-env-sitepackages = "${python-env}/${python-env.sitePackages}";
   prompt = ''\[\033[1;32m\][nix-shell:\w]\$\[\033[0m\] '';
   packages =
     [
-      klayout
+      python-env
+    
+      klayout-app
       coreutils
     ]
     ++ extra-packages;
 in
   devshell.mkShell {
     devshell.packages = packages;
+    env = [
+      {
+        name = "NIX_PYTHONPATH";
+        value = "${python-env-sitepackages}";
+      }
+    ];
     devshell.interactive.PS1 = {
       text = ''PS1="${prompt}"'';
     };
